ca8af19c6e1ab97e9c5247b87358ecd12fcda800
Shanejr <stlazjr0@gmail.com>
2019-02-18 23:37:25 -0800

    Adding automaticly generated files for flutter

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <image_picker/ImagePickerPlugin.h>
+#import <path_provider/PathProviderPlugin.h>
+#import <sqflite/SqflitePlugin.h>
+#import <url_launcher/UrlLauncherPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
+  [FLTUrlLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTUrlLauncherPlugin"]];
+}
+
+@end
